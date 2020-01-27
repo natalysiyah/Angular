f65355166bcd5c41a5ad245bdb0ead467e271449
natalysiyah <natasha.artyuh@gmail.com>
2020-01-27 14:58:05 +0200

    second commit

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact';
+import { CONTACTS } from '../mock-contact';
+import { ContactService } from '../contact.service';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.css']
 })
-export class ContactListComponent implements OnInit {
+export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ContactService: ContactService) { }
+
+  getContacts(): void {
+    this.contacts = this.ContactService.getContacts();
+  }
+
+  img = "https://miro.medium.com/max/1920/1*nb7ULdwbYpi1Y7WBbWZ2mw.png";
 
   ngOnInit() {
+    this.getContacts();
   }
+
+  contacts: Contact[];
+  selectedContact: Contact;
+  onSelect(contact: Contact): void {
+    this.selectedContact = contact;
+  }
+
 
 }
